b1872be0ee83c884c405b7b8d4a830a48df798b7
Stephanie Chang <ischang@ucdavis.edu>
2016-07-29 02:57:08 -0700

    made it so quote doesn't referesh the page every time

--- landing.js
+++ landing.js
@@ -1,18 +1,5 @@
 $(document).ready (function(){
-
-	var quotes =
-	[ 'it\'s like i\'m a developer or something'
-	, 'software engineer by night, snorlax by day'
-	, 'dream big. think often. eat lots.'
-	, 'like jane austen and alan turing made a caffeine addicted baby'
-	, 'git add, git commit, git push, git some sleep'
-	, 'manhole covers are round because they\'re not edgy enough'
-	, 'of course i speak python. hisssssss'
-	];
-
-	var randNum = Math.floor(Math.random() * quotes.length);
-
-	document.getElementById("titleSub").innerHTML = quotes[randNum];
+	quote();
 });
 
 function imageChange(){
@@ -67,4 +54,20 @@ function projects() {
 
 function other(){
 
+}
+
+function quote (){
+	var quotes =
+	[ 'it\'s like i\'m a developer or something'
+	, 'software engineer by night, snorlax by day'
+	, 'dream big. think often. eat lots.'
+	, 'like jane austen and alan turing made a caffeine addicted baby'
+	, 'git add, git commit, git push, git some sleep'
+	, 'manhole covers are round because they\'re not edgy enough'
+	, 'of course i speak python. hisssssss'
+	];
+
+	var randNum = Math.floor(Math.random() * quotes.length);
+
+	document.getElementById("titleSub").innerHTML = quotes[randNum];
 }
